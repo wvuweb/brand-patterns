a2478313cc310e4b57db804006211b62502d91c9
Steven Spriggs <steven.spriggs@gmail.com>
2015-05-21 16:30:53 -0400

    switch from gulp-ruby-sass to node-sass.  with version 2.0.1 release @imports are now working correctly.  Closes #8

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass');
+
+// Sass task
+// Compile Our Sass from the "scss" directory
+gulp.task('sass', function () {
+  gulp.src(['./scss/*.scss','!./scss/_*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./stylesheets'));
+});
+
+gulp.task('default',['sass'], function(){
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
