b901cc251015572b466c8de465ee90ec0d381d08
atglenn <adam.glenn@gmail.com>
2015-06-24 16:59:12 -0400

    Adding mobile search toggle.

--- javascripts/responsive-nav--custom.js
+++ javascripts/responsive-nav--custom.js
@@ -2,3 +2,7 @@ var navigation = responsiveNav(".nav-collapse, .wvu-nav ul", {
   insert: "before",
   transition: 600
 });
+
+$('.inactive-search').click(function() {
+  $(this).toggleClass('active-search');
+});
