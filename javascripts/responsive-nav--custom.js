be74406978bb5647c3434b2a7b60fbf949cb300b
Adam Johnson <adam.b.johnson1@gmail.com>
2015-10-30 14:49:06 -0700

    Separate search nav toggle JS away from responsive-nav--custom.js
    responsive-nav--custom.js doesn’t depend on jQuery and should be in its
    own file. Related to #52.

--- javascripts/responsive-nav--custom.js
+++ javascripts/responsive-nav--custom.js
@@ -2,7 +2,3 @@ var navigation = responsiveNav(".nav-collapse, .wvu-nav ul", {
   insert: "before",
   transition: 600
 });
-
-$('.inactive-search').click(function() {
-  $(this).toggleClass('active-search');
-});
---
+++ javascripts/wvu-masthead-nav-search-toggle.js
@@ -0,0 +1,5 @@
+// Required JS for "WVU Masthead w/Nav + Search Toggle"
+// Dependencies: jQuery
+$('.inactive-search').click(function() {
+  $(this).toggleClass('active-search');
+});
