741915f4088ee0ea3f5ca000c1f2b3d09a05b4ea
Adam Johnson <adam.b.johnson1@gmail.com>
2018-07-02 15:11:29 -0400

    Add browsersync to gulpfile.
    Browser Sync injects any Sass/CSS changes and auto-reloads your browser on save for HTML, JS & YAML files.
    
    Adding browsersync because it's been a staple for years. It's an added dependency, but can be easily removed if you don't want to use it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 'use strict';
 
 var gulp          = require('gulp'),
+    browserSync   = require('browser-sync').create(),
     sass          = require('gulp-sass');
 
-// Sass task
-// Compile Our Sass from the "scss" directory
-gulp.task('sass', function () {
-  return gulp.src(['./scss/*.scss','!./scss/_*.scss'])
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('./stylesheets'));
+// Browser Sync
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    proxy: 'localhost:2000',
+    open: false
+  });
 });
 
-gulp.task('default',['sass'], function(){
+// Sass task
+// Compile Our Sass from the 'scss' directory
+gulp.task('sass', function () {
+  return gulp.src(['./scss/*.scss', '!./scss/_*.scss'])
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('./stylesheets'))
+    .pipe(browserSync.stream());
+});
+
+// Use Sass, watch & Browser Sync in the default task. Watching HTML, JS & YAML.
+gulp.task('default', ['sass', 'browser-sync'], function(){
   gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch('views/**/*.html').on('change', browserSync.reload);
+  gulp.watch('**/*.yml').on('change', browserSync.reload);
+  gulp.watch('javascripts/**/*.js').on('change', browserSync.reload);
 });
